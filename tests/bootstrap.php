4d62a934c9318b3c8a98ea06dd64037151373f20
solvire <stevenjscott@gmail.com>
2015-11-11 10:31:11 -0800

    dotenv now part of testing environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,6 @@ if(!defined('APPLICATION_NAME')) define('APPLICATION_NAME','solvire-utilities');
 
 require_once realpath( __DIR__ . '/../vendor/autoload.php');
 require_once realpath( __DIR__ . '/BaseTestCase.php' );
+
+$dotenv = new Dotenv\Dotenv(realpath(__DIR__ . "/../"));
+$dotenv->load();
